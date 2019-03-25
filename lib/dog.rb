90d8a860ef6a0be1712d4b5caaf098507c3ccf37
Kelly Sciandra <kellysciandradev@icloud.com>
2019-03-25 20:25:08 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,17 @@
+class Dog 
+  def name= (full_name)
+    @name= full_name
+  end 
+    def name 
+      @name 
+    end 
+  
+  def breed= (full_breed)
+    @breed= full_breed
+  end 
+  
+    def breed
+     @breed 
+  end 
+  
+end 
---
+++ lib/person.rb
@@ -0,0 +1,18 @@
+class Person
+  
+  def name= (full_name)
+    @name= full_name 
+  end 
+  
+    def name 
+      @name 
+    end 
+    
+  def job= (full_job)
+    @job= full_job
+  end 
+  
+    def job
+      @job
+    end 
+end 
