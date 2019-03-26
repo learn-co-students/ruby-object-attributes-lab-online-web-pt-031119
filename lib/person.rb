4d5575ec5ad470b7e0095c64682ad87102db4b9d
Ryan <kerneltux0@gmail.com>
2019-03-26 08:34:58 -0600

    Done.

---
+++ lib/person.rb
@@ -0,0 +1,19 @@
+class Person
+
+  def name=(pers_name)
+    @name = pers_name
+  end
+
+  def name
+    @name
+  end
+
+  def job=(title)
+    @job = title
+  end
+
+  def job
+    @job
+  end
+
+end
